6a91224e9971fb4e293620b0dde87c624896ce11
Teodoro <teodoropartida1@gmail.com>
2026-02-12 23:38:47 -0600

    Modified image service to account for preserving file type

--- image_service.py
+++ image_service.py
@@ -11,39 +11,30 @@ def image_channel(source_folder: str, destination_folder: str, file: UploadFile,
         # this is for setup of source and destinTION
         os.makedirs(source_folder, exist_ok=True) #access and define source_folder this is why we import os, 2nd arg is for logic
         os.makedirs(destination_folder, exist_ok=True) #access and define source_folder this is why we import os
-
+        
         # Check file.filename not path.filename to verify extension
         file_type = file.filename.split(".")[-1].lower()
         if file_type not in ALLOWED:
-            return False, "Unsupported file type"
+            return (False, "Unsupported file type") #boolean needed for Exception
 
-        image_file = file.file.read() # has to be
-        image = Image.open(io.BytesIO(image_file)).convert("RGBA")  # io is needed
+
+        # Save original preserve file_type with fole_type
+        image_bytes = file.file.read() # has to be
+        image_file_path = os.path.join(source_folder, f"{image_name}{file_type}")
+        image.save(image_file_path, {file_type})
+
+
+        image = Image.open(io.BytesIO(image_bytes)).convert("RGBA")  # io is needed
         image_name = os.path.splitext(file.filename)[0]
 
-        # Save original
-        original_path = os.path.join(source_folder, f"{image_name}{file_type}")
-        image.save(original_path, {file_type})
 
         # Check logic based on img_type argument
         if image_type == "original": # jpeg, jpg, png converts to png
             pass
 
         elif image_type == "thumbnail":
-            processed = image.copy()
-            processed.thumbnail((128, 128))
-            output_path = os.path.join(destination_folder, f"{image_name}_thumb.png")
-            processed.save(output_path, "PNG")
 
-        else:
-            return False, "Invalid image_type, choose original or thumbnail."
-
-        return True, f"Upload successful"
-
-    except Exception as e:
-        return False, str(e)
-
-
+            pass
 
 @app.post("/upload-image")
 async def upload_image(file: UploadFile = File(...)):
